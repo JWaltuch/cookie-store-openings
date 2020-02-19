9d068b206c1057bc0edfce6d0023871345df5259
JWaltuch <jenna.waltuch@gmail.com>
2020-02-19 16:50:31 -0500

    feat: grabs and displays eatery data

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //express imports
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/', (req, res, next) => {
-  res.send('Hello World')
+app.use('/', async (req, res, next) => {
+  try {
+    let {data} = await axios.get(
+      'https://www.nycgovparks.org/bigapps/DPR_Eateries_001.json'
+    )
+    res.send(data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 app.listen(8080)
